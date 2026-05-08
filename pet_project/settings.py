89aee04913de08bb868ace8c10a3b51d674477a4
Alina <Lalika31253@gmail.com>
2026-05-07 23:03:01 -0600

    Update settings.pu file

--- pet_project/settings.py
+++ pet_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'adoption',
 ]
 
 MIDDLEWARE = [
@@ -115,3 +116,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# Authentication
+# Where to send unauthenticated users who hit a protected page
+LOGIN_URL = '/login/'
+
+# Where to send users after a successful login
+LOGIN_REDIRECT_URL = '/'
+
+# Where to send users after logout
+LOGOUT_REDIRECT_URL = '/login/'
